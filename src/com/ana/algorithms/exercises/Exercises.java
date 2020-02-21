7a66633dea4f3d967e3fba09965f3f140ca9995e
ENDAVA\ana.pesic <ana.pesic@endava.com>
2020-02-21 14:01:10 +0100

    some exercises

--- src/com/ana/algorithms/exercises/Exercises.java
+++ src/com/ana/algorithms/exercises/Exercises.java
@@ -2,6 +2,7 @@ package com.ana.algorithms.exercises;
 
 public class Exercises {
     private static int inputInteger;
+    private static String reversed;
 
     public static String isPalindrome(String inputString) {
         int i = 0, j = inputString.length() - 1;
@@ -49,6 +50,31 @@ public class Exercises {
         return "Number " + input + " is not Armstrong number!";
     }
 
+    public static String reverse(String str) {
+        reversed = "";
+        reverseString(str);
+        return str + " reversed is " + reversed;
+    }
+
+    public static String findGCD(int num1, int num2) {
+        int gcd = 1, div = 2;
+
+        if (num1 == 0 || num2 == 0) return "DIVISION BY ZERO!";
+
+        if (num1 > num2 && (num1 % num2) == 0) gcd = num2;
+        else if (num2 % num1 == 0) gcd = num1;
+
+        else {
+            while ((num1 % div == 0) && (num2 % div == 0) && isPrime(div)) {
+                num1 /= div;
+                num2 /= div;
+                gcd *= div;
+                div = 2;
+            }
+        }
+
+        return "GCD for " + num1 + " and " + num2 + " is: " + gcd;
+    }
 
 
 
@@ -64,5 +90,21 @@ public class Exercises {
 
     private static int getDigit(int i) {
         return (inputInteger % (int)Math.pow(10 , i)) / (int)Math.pow(10 , (i - 1));
+    }
+
+    private static void reverseString(String str) {
+        if (str.length() !=  0) {
+            String prefix = str.substring(0,1);
+
+            str = str.substring(1);
+
+            reverseString(str);
+
+            reversed += prefix;
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        return true;
     }
 }
