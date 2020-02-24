eab577238927303185e94a5f33633ff1a562cf61
ENDAVA\ana.pesic <ana.pesic@endava.com>
2020-02-24 16:20:59 +0100

    some exercises

--- src/com/ana/algorithms/Main.java
+++ src/com/ana/algorithms/Main.java
@@ -5,8 +5,8 @@ import com.ana.algorithms.exercises.Exercises;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {4,10,8,7,6,5,3,12,14};
-        //int[] arr = {462, 273, 1465, 722, 383};
+        int[] arr = {4,10,8,4,6,5,8,10,4};
+        int[] arr2 = {462, 273, 1465, 722, 383};
 
         /*
         SortingAlgorithms sortingAlgorithms = new SortingAlgorithms(arr);
@@ -22,10 +22,29 @@ public class Main {
         System.out.println(v.encrypt(vs, "ana"));
         */
 
-        System.out.println(Exercises.isPalindrome("nije palindrom"));
-        System.out.println(Exercises.isPalindrome(1001));
-        System.out.println(Exercises.isArmstrongNumber(153));
-        System.out.println(Exercises.isArmstrongNumber(135));
+        //System.out.println(Exercises.isPalindrome("nije palindrom"));
+        //System.out.println(Exercises.isPalindrome(1001));
+        //System.out.println(Exercises.isArmstrongNumber(153));
+        //System.out.println(Exercises.isArmstrongNumber(135));
+        //System.out.println(Exercises.reverse("java"));
+
+        /*
+        Exercises.printLeftPyramid(6);
+
+        System.out.println();
+
+        Exercises.printReversedLeftPyramid(6);
+
+        System.out.println();
+
+        Exercises.printRightPyramid(6);
+
+        System.out.println();
+
+        Exercises.printReversedRightPyramid(6);
+        */
+
+        //Exercises.removeDuplicatesV1(arr);
 
         //f.calculate(6);
 
@@ -34,5 +53,23 @@ public class Main {
 
         //System.out.println(sortingAlgorithms.toString());
         //System.out.println(fn.toString());
+
+        //Exercises.squareRoot(25);
+
+        //Exercises.squareRoot(81);
+
+        //Exercises.squareRoot(50);
+
+        //Exercises.squareRoot(5);
+
+        //Exercises.squareRoot(100);
+
+        //Exercises.reverseInPlace(arr2);
+
+        //Exercises.reverseWordsInSentence("Write a program to check if two given String is Anagram of each other.");
+
+        //Exercises.isAnagram("eleven plus two", "twelve plus one");
+
+        Exercises.reverseNumber(123456);
     }
 }
